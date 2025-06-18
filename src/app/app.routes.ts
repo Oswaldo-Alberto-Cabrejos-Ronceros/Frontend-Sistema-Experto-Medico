c9f73a9672d90bb55d55bddac22b77fc300132ad
Oswaldo <U22209742@utp.edu.pe>
2025-06-17 22:15:40 -0500

    feat:Desarrollo de la vista registro

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,5 +1,6 @@
 import { Routes } from '@angular/router';
 import { LoginPageComponent } from '../pages/login-page/login-page.component';
+import { RegisterPageComponent } from '../pages/register-page/register-page.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: '/auth/login', pathMatch: 'full' },
@@ -7,8 +8,17 @@ export const routes: Routes = [
     path: 'auth',
     children: [
       {
+        path: '',
+        redirectTo: 'login',
+        pathMatch: 'full',
+      },
+      {
         path: 'login',
         component: LoginPageComponent,
+      },
+      {
+        path: 'register',
+        component: RegisterPageComponent,
       },
     ],
   },
---
+++ src/pages/register-page/register-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { RegisterCardComponent } from './components/register-card/register-card.component';
+
+@Component({
+  selector: 'app-register-page',
+  imports: [RegisterCardComponent],
+  templateUrl: './register-page.component.html',
+  styleUrl: './register-page.component.scss'
+})
+export class RegisterPageComponent {
+
+}
