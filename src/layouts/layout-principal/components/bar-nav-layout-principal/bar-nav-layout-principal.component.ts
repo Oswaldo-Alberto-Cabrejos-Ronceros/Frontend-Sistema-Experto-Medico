d243816b07e6d442822634f7d714586548caac71
Oswaldo <U22209742@utp.edu.pe>
2025-06-19 17:02:11 -0500

    feat:Implementacion de menu en perfil

--- src/layouts/layout-principal/components/bar-nav-layout-principal/bar-nav-layout-principal.component.ts
+++ src/layouts/layout-principal/components/bar-nav-layout-principal/bar-nav-layout-principal.component.ts
@@ -4,30 +4,84 @@ import { MenubarModule } from 'primeng/menubar';
 import { MenuItem } from 'primeng/api';
 import { RouterModule } from '@angular/router';
 import { RippleModule } from 'primeng/ripple';
+import { TieredMenuModule } from 'primeng/tieredmenu';
+import { ConfirmDialog } from 'primeng/confirmdialog';
+import { ConfirmationService } from 'primeng/api';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-bar-nav-layout-principal',
-  imports: [ButtonModule, MenubarModule,RouterModule,RippleModule],
+  imports: [
+    ButtonModule,
+    MenubarModule,
+    RouterModule,
+    RippleModule,
+    ConfirmDialog,
+    TieredMenuModule,
+    CommonModule,
+  ],
   templateUrl: './bar-nav-layout-principal.component.html',
   styleUrl: './bar-nav-layout-principal.component.scss',
+  providers: [ConfirmationService],
 })
 export class BarNavLayoutPrincipalComponent {
+  //inyectamos confirmationService
+  constructor(private confirmationService: ConfirmationService) {}
   items: MenuItem[] = [
     {
       label: 'Sintomas',
-      route:'/principal/sintomas'
+      route: '/principal/sintomas',
     },
     {
       label: 'Historial',
-      route:'/principal/historial'
+      route: '/principal/historial',
     },
     {
       label: 'Hospitales',
-      route:'/principal/hospitales'
+      route: '/principal/hospitales',
     },
     {
       label: 'Ayuda',
-      route:'/principal/ayuda'
+      route: '/principal/ayuda',
     },
   ];
+
+  perfilMenuItems: MenuItem[] = [
+    {
+      label: 'Ver perfil',
+      icon: 'pi pi-user',
+      route: '/auth',
+    },
+    {
+      label: 'Cerrar sesión',
+      icon: 'pi pi-sign-out',
+    },
+  ];
+
+  //for confirm log out
+  confirmLogOut(event: Event) {
+    this.confirmationService.confirm({
+      target: event.target as EventTarget,
+      message: 'Estás seguro que quieres cerrar sesión?',
+      header: 'Cerrar sesión',
+      closable: true,
+      closeOnEscape: true,
+      icon: 'pi pi-exclamation-triangle',
+      rejectButtonProps: {
+        label: 'No',
+        severity: 'secondary',
+        outlined: true,
+      },
+      acceptButtonProps: {
+        label: 'Si',
+        severity: 'danger',
+      },
+      accept: () => {
+        console.log('Si');
+      },
+      reject: () => {
+        console.log('No');
+      },
+    });
+  }
 }
