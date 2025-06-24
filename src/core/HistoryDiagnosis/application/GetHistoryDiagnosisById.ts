767126bc0f15e4b4ed6e76a991e1e821bc979eda
Oswaldo <U22209742@utp.edu.pe>
2025-06-23 20:50:45 -0500

    feat:Desarrollo de la logica del modulo HistoryDiagnosis

---
+++ src/core/HistoryDiagnosis/application/GetHistoryDiagnosisById.ts
@@ -0,0 +1,18 @@
+import { Inject, Injectable } from '@angular/core';
+import { HistoryDiagnosisService } from '../domain/services/HistoryDiagnosisService';
+import { Observable } from 'rxjs';
+import { HistoryDiagnosis } from '../domain/models/HistoryDiagnosis';
+
+@Injectable({ providedIn: 'root' })
+export class GetHistoryDiagnosisById {
+  constructor(
+    @Inject(HistoryDiagnosisService)
+    private historyDiagnosisService: HistoryDiagnosisService
+  ) {}
+
+  execute(historiaDiagnosisId: number): Observable<HistoryDiagnosis> {
+    return this.historyDiagnosisService.getHistoryDiagnosisById(
+      historiaDiagnosisId
+    );
+  }
+}
---
+++ src/core/HistoryDiagnosis/domain/models/HistoryDiagnosis.ts
@@ -0,0 +1,6 @@
+export interface HistoryDiagnosis {
+  id: number;
+  fecha: string;
+  user_id: number;
+  diagnostico_id: number;
+}
