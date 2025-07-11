2e30e19e3c9260a0acd0c690ed75cafa12d796ee
Oswaldo <U22209742@utp.edu.pe>
2025-07-11 17:36:27 -0500

    feat:inclusion de toast

--- src/pages/login-page/login-page.component.ts
+++ src/pages/login-page/login-page.component.ts
@@ -5,9 +5,11 @@ import { AuthenticationServiceImpl } from '../../core/Authentication/infrastruct
 import { Login } from '../../core/Authentication/application/Login';
 import { LoginRequest } from '../../core/Authentication/domain/models/Authentication';
 import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { Toast } from 'primeng/toast';
 @Component({
   selector: 'app-login-page',
-  imports: [LoginCardComponent],
+  imports: [LoginCardComponent,Toast],
   templateUrl: './login-page.component.html',
   styleUrl: './login-page.component.scss',
   providers:[
@@ -15,11 +17,12 @@ import { Router } from '@angular/router';
       provide:AuthenticationService,
       useClass:AuthenticationServiceImpl
     },
-    Login
+    Login,
+    MessageService
   ]
 })
 export class LoginPageComponent {
-  constructor(private login:Login,private router:Router){}
+  constructor(private login:Login,private router:Router,private messageService: MessageService){}
 
 //funcion para logear
   userLogin=(loginRequest:LoginRequest)=>{
@@ -28,9 +31,20 @@ export class LoginPageComponent {
       next:(data)=>{
         console.log(data)
         this.router.navigate(['/principal'])
+        this.show('success','Bienvido','Hola de nuevo')
+      },
+      error:(error)=>{
+        console.error(error)
+        this.show('warn','Error','Credenciales invalidas o no tiene usuario')
       }
     })
 
 
   }
+
+  //for show toast
+
+      show(severity:string,message:string,summary:string) {
+        this.messageService.add({ severity:severity, summary: summary, detail: message, life: 3000 });
+    }
 }
